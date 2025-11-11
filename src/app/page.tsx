4fedaccd6e1154e6b26cb7931487ef1874f6ba8c
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-11-11 11:39:52 -0700

    adding features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Container } from '@/components/container'
+import { FeatureSection } from '@/components/feature-section'
 import { Hero } from '@/components/hero'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <div className="my-24 md:my-32">
-        <Container>
-          <h1>Hello World</h1>
-        </Container>
-      </div>
+      <FeatureSection />
     </main>
   )
 }
